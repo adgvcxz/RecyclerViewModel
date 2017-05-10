85d1bc2963ad8a875a524b0abe24a79777248bed
adgvcxz <344292600@qq.com>
2017-05-10 19:38:46 +0800

    init project

---
+++ RecyclerViewModel/app/src/main/java/com/adgvcxz/recyclerviewmodel/simple/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.adgvcxz.recyclerviewmodel.simple
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * zhaowei
+ * Created by zhaowei on 2017/5/10.
+ */
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
